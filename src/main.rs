787dcc805ab71f8553d06a5d4db6c22d4904e0c0
Niko Steinhoff <niko.steinhoff@gmail.com>
2020-03-18 14:37:23 +0100

    Improve error handling by using the Result type

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use std::process;
 use std::env;
 use std::fs;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        panic!("Not enough arguments");
-    }
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for '{}'", config.query);
     println!("In file '{}'", config.filename);
@@ -24,10 +25,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Self {
+    fn new(args: &[String]) -> Result<Self, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
